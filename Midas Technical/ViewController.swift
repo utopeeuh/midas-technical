6012612435570938ebf094262c2eee5b2f510044
Uto <daffamadeo@gmail.com>
2023-03-08 13:08:10 +0700

    Initial Commit

---
+++ Midas Technical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Midas Technical
+//
+//  Created by Tb. Daffa Amadeo Zhafrana on 08/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
